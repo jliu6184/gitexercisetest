cfc6744720954ec77e3d8d4fcb62d2393f7858e9
Jiajun Liu <125538432+jliu6184@users.noreply.github.com>
2024-02-12 14:28:57 -0800

    Add files via upload

--- wordle.cpp
+++ wordle.cpp
@@ -3,6 +3,9 @@
 #include <string> 
 #include <iostream>
 #include <fstream>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
@@ -11,22 +14,41 @@ int main()
     /* actual wordle code! */
     fstream words_file;
     words_file.open("words.txt", ios::in);
+	string wordleArray[10];
     if (!words_file) {
 		cout << "No such file";
-	}
-	else {
-		char ch;
+	} else {
+		string ch;
 
-		while (1) {
-			my_file >> ch;
-			if (my_file.eof())
-				break;
-
-			cout << ch;
+		for(int i = 0; 10 > i; i++)
+		{
+            getline(words_file, ch);
+            wordleArray[i] = ch;
+            cout << wordleArray[i];
 		}
 
+		srand(time(0));
+		int randomNumber = rand() % 10;
+		string chosenWord = wordleArray[randomNumber];
+		std::cout << "Random number: " << randomNumber << std::endl;
+		std::cout << "Chosen word: " << chosenWord << std::endl;
+
+		int guessNum = 0; 
+		string inputArray[1000];
+		bool won = false;
+  
+		while (!won) {
+			std::cout << "Type in your guess " << guessNum << ": " << std::endl;
+			cin >> inputArray[guessNum]; 
+			if (inputArray[guessNum] == chosenWord) {
+				won = true;
+			}
+			guessNum++;
+		}
+
+		std::cout << "Congrats! You guessed the word \"" << chosenWord << "\" in " << guessNum << " tries! " << std::endl;
 	}
-	my_file.close();
+	words_file.close();
 	return 0;
 
     /* hello world code! */
